14dd8f5c3e87f54e3087e8b2a6f4b80d3f68818d
Sebastião Júnior <junior@saibweb.com.br>
2023-01-25 16:39:36 -0300

    .

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from 'react';
+import Scanner from './Scanner';
+import Result from './Result';
+
+const App = () => {
+    const [scanning, setScanning] = useState(false);
+    const [results, setResults] = useState([]);
+    const scannerRef = useRef(null);
+
+    return (
+        <div>
+            <button onClick={() => setScanning(!scanning) }>{scanning ? 'Stop' : 'Start'}</button>
+            <ul className="results">
+                {results.map((result) => (result.codeResult && <Result key={result.codeResult.code} result={result} />))}
+            </ul>
+            <span id="resultado">Teste</span>
+            <div ref={scannerRef} style={{position: 'relative', border: '3px solid red'}}>
+                {/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
+                <canvas className="drawingBuffer" style={{
+                    position: 'absolute',
+                    top: '0px',
+                    // left: '0px',
+                    // height: '300px',
+                    // width: '800px',
+                    // border: '3px solid green',
+                }} width="800" height="600" />
+                {scanning ? <Scanner scannerRef={scannerRef} onDetected={(result) => setResults([...results, result])} /> : null}
+            </div>
+        </div>
+    );
+};
+
+export default App;
