eab121c26a392dfae9d0a5e5440430250d822b16
Sebastião Júnior <junior@saibweb.com.br>
2023-01-25 23:42:13 -0300

    ok

--- src/routes/html5qrcodeplugin.jsx
+++ src/routes/html5qrcodeplugin.jsx
@@ -1,7 +1,12 @@
+import { string } from "prop-types";
 import React, { Component } from "react";
 import Html5QrcodePlugin from "../components/Html5QrCodePlugin";
 
 class Html5QrCodePluginPage extends Component {
+  state = {
+    codigo: '',
+  };
+
   constructor(props) {
     super(props);
 
@@ -9,19 +14,20 @@ class Html5QrCodePluginPage extends Component {
     this.onNewScanResult = this.onNewScanResult.bind(this);
   }
   render() {
+    const {codigo} = this.state;
     return (
       <div>
-        <h1>Html5Qrcode React example!</h1>
+        <h1>{codigo !== '' ? codigo : <>Html5Qrcode React example!</>} </h1>
         <Html5QrcodePlugin
           fps={20}
-          qrbox={300}
-          disableFlip={false}
+          disableFlip={true}
           qrCodeSuccessCallback={this.onNewScanResult}
         />
       </div>
     );
   }
   onNewScanResult(decodedText, decodedResult) {
+    this.setState({codigo: decodedText});
     console.log("decodedText", decodedText);
     console.log("decodedResult", decodedResult);
   }
