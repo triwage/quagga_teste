8874b43c504e23db20de3e93aeba351c34a3c0bd
douglasfmSaibWeb <douglas.munaro@saibweb.com.br>
2023-01-31 22:07:45 -0300

    ajustes

--- src/components/Quaggar/index.jsx
+++ src/components/Quaggar/index.jsx
@@ -1,47 +1,57 @@
-import React, { useState, useRef, useEffect } from "react";
-import Scanner from "./Scanner";
-import Result from "./Result";
+// @ts-nocheck
+import { useState, useRef } from "react"
+
+import Result from "./Result"
+import Scanner from "./Scanner"
+
+import DeviceOrientation, { Orientation } from 'react-screen-orientation'
 
 const Quaggar = () => {
-  const [scanning, setScanning] = useState(false);
-  const [results, setResults] = useState([]);
-  const scannerRef = useRef(null);
-  return (
-    <div>
-      <button onClick={() => setScanning(!scanning)}>
-        {scanning ? "Stop" : "Start"}
-      </button>
-      <ul className="results">
-        {results.map(
-          (result) =>
-            result.codeResult && (
-              <Result key={result.codeResult.code} result={result} />
-            )
-        )}
-      </ul>
-      <div
-        id="interactive" className="viewport"
-        ref={scannerRef}
-        style={{ position: "relative", border: "3px solid red" }}
-      >
-        {/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
-        <canvas
-          className="drawingBuffer"
-          style={{
-            position: "absolute",
-            top: "0px",
-          }}
-        />
-        {scanning ? (
-          <Scanner
-            scannerRef={scannerRef}
-            onDetected={(result) => setResults([...results, result])}
-            facingMode="environment"
-          />
-        ) : null}
-      </div>
-    </div>
-  );
-};
+  const [scanning, setScanning] = useState(false)
+  const [results, setResults] = useState([])
+  const scannerRef = useRef(null)
 
-export default Quaggar;
+  return (
+    // @ts-expect-error
+    <DeviceOrientation lockOrientation={'landscape'}>
+      <Orientation orientation={'landscape'} alwaysRender={false}>
+        <div className="h-screen w-screen">
+          <button onClick={() => setScanning(!scanning)}>
+            {scanning ? "Stop" : "Start"}
+          </button>
+          <ul className="results">
+            {results.map(
+              (result) =>
+                result.codeResult && (
+                  <Result key={result.codeResult.code} result={result} />
+                ),
+            )}
+          </ul>
+          <div
+            id="interactive" className="viewport"
+            ref={scannerRef}
+            style={{ position: "relative" }}
+          >
+            {/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
+            <canvas
+              className="drawingBuffer"
+              style={{
+                position: "absolute",
+                top: "0px",
+              }}
+            />
+            {scanning ? (
+              <Scanner
+                scannerRef={scannerRef}
+                onDetected={(result) => setResults([...results, result])}
+                facingMode="environment"
+              />
+            ) : null}
+          </div>
+        </div>
+      </Orientation>
+    </DeviceOrientation>
+  )
+}
+
+export default Quaggar
